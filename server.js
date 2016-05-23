f5be85430959925ece66678648edae71d8a69394
calvinchankf <chan9118kin@gmail.com>
2016-05-24 00:35:18 +0800

    new branch for develop

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ if (isDeveloping) {
   app.get('*', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
   });
+  
 } else {
   app.use(express.static(__dirname + '/dist'));
   app.get('*', function response(req, res) {
